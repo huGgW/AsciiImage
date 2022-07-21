5d95120a98ba797d53375419ef3efd2ac58df0db
huGgW <whjoon0225@naver.com>
2022-07-22 00:59:23 +0900

    error on deriving

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,47 @@
-use std::fs::File;
-use std::io::prelude::*;
+use clap::{Parser, SubCommand};
 
 mod to_ascii;
 mod save_ascii;
 mod print_ascii;
+
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(SubCommand)]
+enum Commands {
+    #[clap(subcommand)]
+    save(Save),
+    #[clap(subcommand)]
+    print(Print),
+}
+
+#[derive(SubCommand)]
+struct Save {
+    #[clap(short, long, default_value_t = String::from("./images/image.png"))]
+    imgdir: String,
+    #[clap(short, long, default_value_t = String::from("./result/result.txt"))]
+    savedir: String,
+    #[clap(short, long, default_value_t = 16u32)]
+    size: u32,
+    #[clap(short, long, action)]
+    complex: bool,
+    #[clap(short, long, action)]
+    invert: bool,
+}
+
+#[derive(SubCommand)]
+struct Print {
+    #[clap(short, long, default_value_t = String::from("./images/image.png"))]
+    imgdir: String,
+    #[clap(short, long, default_value_t = 16u32)]
+    size: u32,
+    #[clap(short, long, action)]
+    complex: bool,
+    #[clap(short, long, action)]
+    invert: bool,
+}
 
 fn main() {
     let img_path = "images/ubuntu.png";
